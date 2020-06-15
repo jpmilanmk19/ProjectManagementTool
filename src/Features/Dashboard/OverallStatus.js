b87b79b67d10109882038944d85ac2f30b8387c9
Milan <jpmilanmk@hotmail.com>
2020-06-15 14:45:12 +0530

    updated files

--- src/Features/Dashboard/OverallStatus.js
+++ src/Features/Dashboard/OverallStatus.js
@@ -1,32 +1,49 @@
-import React from 'react'
-import { Grid, Segment } from 'semantic-ui-react'
-import './OverallStatus.css'
+import React, { useState, useEffect } from 'react';
+import { Grid, Segment } from 'semantic-ui-react';
+import './OverallStatus.css';
+import axios from 'axios';
 
 export default function OverallStatus() {
+    const [overallStatus,setOverallStatus] =  useState({}) 
+
+    useEffect(()=>{
+        axios.get('https://my-json-server.typicode.com/jpmilanmk19/ProjectManagementTool/overallStatus')
+        .then((response)=>{
+            setOverallStatus(response.data)
+            console.log(overallStatus)
+        })
+        .catch((error)=>{
+
+        })
+    },[])
     return (
         <Segment>
             <Grid divided>
                 <Grid.Column width={4}>
                     <div className="content_center">
-                        Project Count
+                        <p>Project Count</p>
+                        <h1>{overallStatus.projects}</h1>
                     </div>
 
                 </Grid.Column>
                 <Grid.Column width={4}>
                     <div className="content_center">
-                        Resource Count
+                        <p>Resource Count</p>
+                        <h1>{overallStatus.resources}</h1>
+                    </div>
+
+                </Grid.Column>
+                <Grid.Column width={4} className="background_color">
+                    <div className="content_center">
+                       <p> Unallocated Count</p>
+                       <h1>{overallStatus.unallocatedCount}</h1>
                     </div>
 
                 </Grid.Column>
                 <Grid.Column width={4}>
                     <div className="content_center">
-                        Unallocated Count
-                    </div>
-
-                </Grid.Column>
-                <Grid.Column width={4}>
-                    <div className="content_center">
-                        Openings
+                        <p>Openings</p>
+                        <h1>{overallStatus.openings}</h1>
                     </div>
 
                 </Grid.Column>
